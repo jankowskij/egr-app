92b89275b2bffad21ad6e310c0ab113f2bb10b7f
jankowskij <volggalla@gmail.com>
2022-11-29 02:50:11 +0300

    create filetries

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,22 @@
-
+import '../../scss/style.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="MainContainer">
+      <div className="MainToning"></div>
+      <div className="MainHeader">
+        <div className="Logo">
+          <img src="" alt="" />
+          <h3>RNK</h3>
+        </div>
+        <div className="ButtonHeader">
+          <img src="../../image/svg/HamburgMengu.svg" alt="HamburgMenu" className="iconHeader" />
+          <img src="../../image/svg/HamburgMengu.svg" alt="HamburgMenu" className="iconHeader" />
+          <img src="../../image/svg/HamburgMengu.svg" alt="HamburgMenu" className="iconHeader" />
+        </div>
+      </div>
+      <div className="MainFooter"></div>
+      <div>321</div>
     </div>
   );
 }
