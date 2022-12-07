70cc925ee7e991081058b6964078d0fab2a83013
jankowskij <volggalla@gmail.com>
2022-12-08 00:40:37 +0300

    add modal windows for Auth not refact

--- src/components/App/SignInForm.jsx
+++ src/components/App/SignInForm.jsx
@@ -3,11 +3,11 @@
 
 const SignInForm = (props) => {
     return (
-        <>
+        <div className="containerForm">
             <div className="form">
                 <div onClick={props.setNoActive}>Close</div>
             </div>
-        </>
+        </div>
     )
 }
 
